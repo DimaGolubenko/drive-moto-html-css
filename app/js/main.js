38e25b75d420209772c839f703bfe96f4e8b5b1a
Dima Golubenko <dimagolubenko@gmail.com>
2020-07-29 23:06:39 +0300

    finish sales banners slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,18 @@
 $(function(){
 
-
+  $('.sales-banners__slider').slick({
+    dots: true,
+    prevArrow: `
+      <button class="sales-banners__slider-arrow sales-banners__slider-arrow--left">
+        <img src="images/sales-slider-arrow-left.svg" alt="">
+      </button>
+    `,
+    nextArrow: `
+      <button class="sales-banners__slider-arrow sales-banners__slider-arrow--right">
+        <img src="images/sales-slider-arrow-right.svg" alt="">
+      </button>
+    `
+  });
 
   
 });
