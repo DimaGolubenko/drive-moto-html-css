c9450c02b4e0eb336c407ade862945817b19bf6b
Dima Golubenko <dimagolubenko@gmail.com>
2020-08-04 23:34:56 +0300

    finish popular products section, fix products widget

--- app/js/main.js
+++ app/js/main.js
@@ -43,11 +43,30 @@ function initSliders() {
     </button>
   `
   });
+
+  $('.similar-products .products-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true,
+    prevArrow: `
+    <button class="products-slider__arrow products-slider__arrow--left">
+      <img src="images/products-slider-arrow-left.svg" alt="">
+    </button>
+  `,
+  nextArrow: `
+    <button class="products-slider__arrow products-slider__arrow--right">
+      <img src="images/products-slider-arrow-right.svg" alt="">
+    </button>
+  `
+  });
 }
 
 function initTabs() {
   new Tabs('.search-products').init();
   new Tabs('.popular-products', {}, function() {
+    $('.products-slider').slick('setPosition');
+  }).init();
+  new Tabs('.similar-products', {}, function() {
     $('.products-slider').slick('setPosition');
   }).init();
 }
